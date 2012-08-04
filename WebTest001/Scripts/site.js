25907a16682a6fc40868732d1995718317dce162
DL <botchla411@yahoo.com>
2012-08-04 08:31:52 -0500

    knockoutjs example 3

--- WebTest001/Scripts/site.js
+++ WebTest001/Scripts/site.js
@@ -1,14 +1,18 @@
 ﻿var MasterViewModel = function () {
     var self = this;
-    self.price = ko.observable(25.99);
 
-    self.formattedPrice = ko.computed({
-        read: function () {
-            return "$" + self.price().toFixed(2);
-        },
+    self.acceptedNumericValue = ko.observable(123);
+    self.lastInputWasValid = ko.observable(true);
+
+    self.attemptedValue = ko.computed({
+        read: self.acceptedNumericValue,
         write: function (value) {
-            value = parseFloat(value.replace(/[^\.\d]/g, ""));
-            self.price(isNaN(value) ? 0 : value);
+            if (isNaN(value)) {
+                self.lastInputWasValid(false);
+            } else {
+                self.lastInputWasValid(true);
+                self.acceptedNumericValue(value);
+            }
         },
         owner: self
     });

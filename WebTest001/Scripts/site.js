1a7f7e4c245180357dbd73fd1caf6f38aa2e97ef
DL <botchla411@yahoo.com>
2012-08-04 09:18:01 -0500

    working with ko test examples

--- WebTest001/Scripts/site.js
+++ WebTest001/Scripts/site.js
@@ -1,21 +1,14 @@
 ﻿var MasterViewModel = function () {
-    var self = this;
+    var self, function1, function2;
 
-    self.acceptedNumericValue = ko.observable(123);
-    self.lastInputWasValid = ko.observable(true);
-
-    self.attemptedValue = ko.computed({
-        read: self.acceptedNumericValue,
-        write: function (value) {
-            if (isNaN(value)) {
-                self.lastInputWasValid(false);
-            } else {
-                self.lastInputWasValid(true);
-                self.acceptedNumericValue(value);
-            }
-        },
-        owner: self
-    });
+    self = this;
+    self.theValue = ko.observable("");
+    self.function1 = function () {
+        return alert("button1 " + self.theValue());
+    }
+    self.function2 = function () {
+        return alert("button2 " + self.theValue());
+    }
 }
 
 ko.applyBindings(new MasterViewModel());

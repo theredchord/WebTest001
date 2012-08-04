3f87e9e8c5b1ab045bad4b6b9b3ee2f16c6b4bc8
DL <botchla411@yahoo.com>
2012-08-03 19:08:50 -0500

    first commit

---
+++ WebTest001/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebTest001.MvcApplication" Language="C#" %>
---
+++ WebTest001/Scripts/site.js
@@ -0,0 +1,11 @@
+﻿function AppViewModel() {
+    var self = this;
+
+    self.firstName = ko.observable("Dustin");
+    self.lastName = ko.observable("Litorja");
+    self.fullName = ko.computed(function () {
+        return self.firstName() + " " + self.lastName();
+    }, this);
+}
+
+ko.applyBindings(new AppViewModel);

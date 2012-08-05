65b85cb126afaac58c88c3ee18003cefacb45a8c
DL <botchla411@yahoo.com>
2012-08-05 15:22:23 -0500

    working with css data binding

--- WebTest001/Scripts/site.js
+++ WebTest001/Scripts/site.js
@@ -2,20 +2,7 @@
     var self, testVal;
     self = this;
 
-    self.theValue = ko.observable("");
-    self.theArray = ko.observableArray([]);
-    self.arrayAdd = function () {
-        if ($("#theValInput").val().length > 0) {
-            self.theArray.push({
-                value: self.theValue()
-            });
-        } else {
-            alert("You must input a value");
-        }
-    }
-    self.arrayRemove = function () {
-        self.theArray.pop();
-    }
+    self.profitVal = ko.observable("0");
 }
 
 ko.applyBindings(new MasterViewModel());

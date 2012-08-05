c954f53afbd8ce3ae3ad8b996f8bbc8a29483674
DL <botchla411@yahoo.com>
2012-08-05 09:32:17 -0500

    working with data binding with arrays and input values

--- WebTest001/Scripts/site.js
+++ WebTest001/Scripts/site.js
@@ -1,26 +1,13 @@
 ﻿var MasterViewModel = function () {
     var self, testVal;
-
     self = this;
-    testVal = 0;
-    self.theArray = ko.observableArray([
-        { value: "hello" },
-        { value: "hello2" },
-        { value: "hello3" },
-        { value: "hello4" }
-    ]);
-    self.function1 = function () {
-        $.each(self.theArray(), function (index, value) {
-            alert(value.value);
-        }
-        );
-    }
-    self.function2 = function () {
-        self.theArray().push({
-            value: "test" + testVal
+
+    self.theValue = ko.observable("");
+    self.theArray = ko.observableArray([]);
+    self.arrayAdd = function () {
+        self.theArray.push({
+            value: self.theValue()
         });
-        alert("Added: " + testVal);
-        testVal += 1;
     }
 }
 

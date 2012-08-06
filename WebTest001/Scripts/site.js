3a37ddf09e743e532b4999265895a0dd36a1ab79
Dustin Litorja <botchla411@yahoo.com>
2012-08-06 10:30:38 -0500

    added ko.observableArray demonstration and validation

--- WebTest001/Scripts/site.js
+++ WebTest001/Scripts/site.js
@@ -2,6 +2,23 @@
     var self;
     self = this;
 
+    //Array-related items
+    self.theValue = ko.observable("");
+    self.theArray = ko.observableArray([]);
+    self.addItem = function () {
+        var isValidInput = $.trim(self.theValue()).length > 0;
+
+        if (!isValidInput) {
+            return alert("You must enter a value.");
+        } else {
+            return self.theArray.push({ value: self.theValue() });
+        }
+    }
+    self.removeItem = function () {
+        return self.theArray.pop();
+    }
+
+    //Checkbox-related items
     self.displayMessage = ko.observable(false);
 }
 
